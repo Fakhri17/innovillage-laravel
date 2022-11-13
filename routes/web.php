ba686002868f85e8a0066a4342d56f3f5fc65ccf
Renaldyp <37020760+renaldypermana@users.noreply.github.com>
2022-11-13 22:25:14 +0700

    done hehe

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,11 @@ Route::get('/login', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'view']);
 Route::get('/dashboard/setting', [SettingController::class, 'view']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
